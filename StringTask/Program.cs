372549623a89fc73ed032ee61a66ae5bbf152bbc
Sasha <alecsand.unit@gmail.com>
2024-09-29 20:14:48 +0300

    Add project files.

---
+++ StringTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace CSharp.String
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Введите арефметическое выражение: ");
+                string str = Console.ReadLine();
+                string[] arrayOfString = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                int rezult = int.Parse(arrayOfString[0]);
+                for (int i = 0; i < arrayOfString.Length; i++)
+                {
+                    int tamp = 0;
+                    if (arrayOfString[i] == "+")
+                    {
+                        tamp = int.Parse(arrayOfString[i + 1]);
+                        rezult += tamp; 
+                    }
+                    else if (arrayOfString[i] == "-")
+                    {
+                        tamp = int.Parse(arrayOfString[i + 1]); 
+                        rezult -= tamp;
+                    }
+                }
+                Console.WriteLine("Ответ: " + rezult);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine (ex.Message); 
+            }
+        }
+    }
+}
---
+++ StringTask2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace CSharp.String
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Введите текст: ");
+                string str = Console.ReadLine();
+                string[] arrayOfString = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string rezult = "";
+                for (int i = 0; i < arrayOfString.Length; i++)
+                {
+                    string temp = arrayOfString[i].Trim();
+                    temp = char.ToUpper(temp[0]) + temp.Substring(1);
+                    rezult += temp + " ";
+                }
+                Console.WriteLine(rezult);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);   
+            }
+
+        }
+    }
+}
